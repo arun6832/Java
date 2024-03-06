fa6457ded42bd2a2d04252ea3a87a2bb260da972
ARUN M <arun.m@msds.christuniversity.in>
2024-03-07 00:11:49 +0530

    update

---
+++ msg.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class msg{
+
+public static void main(String [] args)
+{
+
+	int t=19;
+	if (t<12)
+	{
+		System.out.println("Good Morning");
+	}
+	else if (t<18)
+	{
+		System.out.println("Good Afternoon");
+	}
+	else
+	{
+		System.out.println("Good Evening");
+	}
+}
+
+}
