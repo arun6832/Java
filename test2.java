b1bfe08a9207b9feeada800675c2918fffd3a543
ARUN M <arun.m@msds.christuniversity.in>
2024-03-17 16:53:38 +0530

    update

--- test2.java
+++ test2.java
@@ -1,30 +1,39 @@
 import java.util.Scanner;
 
-public class test2{
+public class test2 {
     
     static Scanner scan = new Scanner(System.in);
 
-    static void EvenOdd() {
+    static boolean isPrime() {
         System.out.println("Enter a number:");
         int a = Integer.parseInt(scan.nextLine());
-        if (a % 2 == 0) {
-            System.out.println("It's an Even Number.");
-        } else {
-            System.out.println("It's an Odd Number.");
+        
+                if (a <= 1) {
+            System.out.println("It's not a Prime number");
+            return false;
         }
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                System.out.println("It's not a Prime number");
+                return false;
+            }
+        }
+
+        System.out.println("It's a Prime number");
+        return true;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int choice;
         do {
             System.out.println("Enter Your Choice:");
-            System.out.println("1. Check if it is Even or Odd");
+            System.out.println("1. Check if it is Prime Number or Not");
             System.out.println("2. Exit");
             choice = Integer.parseInt(scan.nextLine());
 
             switch (choice) {
                 case 1:
-                    EvenOdd();
+                    isPrime();
                     break;
                 case 2:
                     System.out.println("Exiting...");
